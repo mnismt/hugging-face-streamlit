ce9ed58b1b0cabaff38ec5e015530369673dce6c
Thanh Minh <thanhgamedev@gmail.com>
2022-06-21 05:08:42 +0000

    feat: add sentiment result

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import streamlit as st
 from processing import CategorizeReviewTextLabel
 from label import CATEGORIES
 import pandas as pd
+from review_text import ReviewText
 
 
 @st.experimental_memo
@@ -19,12 +20,30 @@ labels = st.multiselect(
 )
 
 if st.button('Process'):
+
     if not review_text:
         st.error('Please input the review text')
         st.stop()
     if len(labels) < 1:
         st.error('Please choose at least one label')
         st.stop()
+
+    review_instance = ReviewText(review_text)
+
+    st.subheader('Sentiment Analysis')
+    review_sentiment = 'Negative'
+    if review_instance.sentiment == 0:
+        review_sentiment = 'Positive'
+    st.markdown("By review text: **{0}**".format(review_sentiment))
+
+    st.write("By each sentence: ")
+
+    def get_sentiment_text(
+        sentiment): return "Positive" if sentiment == 0 else "Negative"
+    sentences_sentiment_text = ["- {0}: **{1}**".format(sentence.text,
+                                                        get_sentiment_text(sentence.sentiment)) for sentence in review_instance.sentences]
+    st.markdown('\n'.join(sentences_sentiment_text))
+
     final_keywords = []
     results_df = pd.DataFrame({'label': labels})
     for idx_label, label in enumerate(labels):
