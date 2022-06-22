e24fb6d94bda8b30288b60463651063d01af0626
Thanh Minh <thanhgamedev@gmail.com>
2022-06-22 14:23:30 +0000

    feat: add keep_or_delete util

--- src/utils.py
+++ src/utils.py
@@ -6,3 +6,22 @@ def flatten(A):
         else:
             rt.append(i)
     return rt
+
+
+def keep_or_delete_label(review_text, DELETE_WORDS, KEEP_WORDS):
+    DELETE_WORDS = flatten(DELETE_WORDS)
+    KEEP_WORDS = flatten(KEEP_WORDS)
+    is_keep = True
+
+    for delete_word in DELETE_WORDS:
+        found = delete_word in review_text
+        review_text = review_text.replace(delete_word, '')
+        if found:
+            is_keep = False
+
+    for keep_word in KEEP_WORDS:
+        if keep_word in review_text:
+            is_keep = True
+            break
+
+    return is_keep
